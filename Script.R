4e07ab05972f92a20fd10d33a568cc4b3523f847
Leonardo Lecci <llecci@student.hult.edu>
2022-06-28 23:01:25 -0700

    update to the three biggest owned stock by the fund

--- Script.R
+++ Script.R
@@ -9,15 +9,15 @@
 library(quantmod)
 
 # Step 1: pulling in pricing data
-#Well's Fargo
-stock1 <- getSymbols("WFC", auto.assign = FALSE)
-# S&P500 via SPY
-stock2 <- getSymbols("SPY", auto.assign = FALSE)
-# US bonds via AGG
-fixed_income <- getSymbols("AGG", auto.assign = FALSE)
+# Novo-Nordisk A/S ADS (NVO)
+stock1 <- getSymbols("NVO", auto.assign = FALSE)
+# Tesla (TSLA)
+stock2 <- getSymbols("TSLA", auto.assign = FALSE)
+# Kroger Co. (KR)
+stock3 <- getSymbols("KR", auto.assign = FALSE)
 
 # Step 2: combining the data frames gathered
-joined_prices <- merge.xts(stock1, stock2, fixed_income)
+joined_prices <- merge.xts(stock1, stock2, stock3)
 
 # Step 3: pulling in only adjusted prices
 joined_prices_only <- joined_prices[,c(6,12,18)]
