b905dba60ceaa521a9d3f07b5d62646493828d96
Leonardo Lecci <llecci@student.hult.edu>
2022-06-29 10:22:21 -0700

    Added 29 biggest stock held by the Renaissance fund Added in the loop code to calculate positions of columns for the adj. prices for each stock

--- Script.R
+++ Script.R
@@ -13,18 +13,23 @@ library(quantmod)
 # Create loop for code efficiency
 # Create vector with all the symbols we want to get
 
-stocks_vector <- c("NVO", "TSLA", "KR")
+stocks_vector <- c("NVO", "TSLA", "KR", "GILD", "VRSN", "AAPL", "AMD", "TEAM", "ZM", "AMZN", "META", "HSY", "MRNA", "FNV", "GOOGL", "MCD", "MOH", "ABNB", "HD", "MNST", "VZ", "XOM", "UTHR", "ABMD", "VOD", "VRTX", "GSK", "CL", "DPZ")
 length_vector <- length(stocks_vector)
 
 # Creating the actual loop
-#initializing joined_prices df
+#initializing joined_prices df, and vector to calculate positions for pulling only adjusted prices
 
 joined_prices <- NULL
+adj_position <- c(NULL)
+
 for(i in 1:length_vector){
   stock_values <- NULL
   stock_values <- getSymbols(stocks_vector[i], auto.assign = FALSE)
   joined_prices <- cbind(joined_prices, stock_values)
+# We know adj. price is in the 6th column for each stock
+# so we multiply each cycle by 6 to get the positions for each stock
+  adj_position <- append(adj_position, i*6)
 }
 
 # Step 2: pulling in only adjusted prices
-joined_prices_only <- joined_prices[,c(6,12,18)]
+joined_prices_only <- joined_prices[,adj_position]
