5a4b89efad95f5ea66f91f3b55dfa98724790ec2
Leonardo Lecci <llecci@student.hult.edu>
2022-06-30 14:17:23 -0700

    Adding code to retrieve the biggest 855 stock held by the fund by importing data from csv file

--- Script.R
+++ Script.R
@@ -8,23 +8,31 @@
 
 library(quantmod)
 
-
 # Step 1: pulling in pricing data and combining the data frames gathered
-# Create loop for code efficiency
-# Create vector with all the symbols we want to get
 
-stocks_vector <- c("NVO", "TSLA", "KR", "GILD", "VRSN", "AAPL", "AMD", "TEAM", "ZM", "AMZN", "META", "HSY", "MRNA", "FNV", "GOOGL", "MCD", "MOH", "ABNB", "HD", "MNST", "VZ", "XOM", "UTHR", "ABMD", "VOD", "VRTX", "GSK", "CL", "DPZ")
-length_vector <- length(stocks_vector)
+# Import portfolio of Renaissance Technology from csv
+# Deleted all symbols with class type (.A .B .WS), they didn't hold a big share of total portfolio
+# Deleted all the stock with a % of portfolio < 0.02, the stocks represented here amount to ~60% of the portfolio
+portfolio <- read.csv("~/Library/Mobile Documents/com~apple~CloudDocs/Hult/Wealth Mngmt/InvestmentPortfolioOptimization/RENAISSANCE-TECHNOLOGIES-LLC-transactions-Q1 2022.csv")
+dim_portfolio <- dim(portfolio)
+dim_portfolio <- dim_portfolio[1]
+stock_vector <- c(NULL)
 
-# Creating the actual loop
+# Create vector with the biggest 855 stock held by the fund
+for(p in 1:dim_portfolio){
+  stock_vector <- append(stock_vector, portfolio[p,2])
+}
+length_vector <- length(stock_vector)
+
 #initializing joined_prices df, and vector to calculate positions for pulling only adjusted prices
-
 joined_prices <- NULL
 adj_position <- c(NULL)
 
+
+# loop to get alll the values of stock through quantmod
 for(i in 1:length_vector){
   stock_values <- NULL
-  stock_values <- getSymbols(stocks_vector[i], auto.assign = FALSE)
+  stock_values <- getSymbols(stock_vector[i], auto.assign = FALSE)
   joined_prices <- cbind(joined_prices, stock_values)
 # We know adj. price is in the 6th column for each stock
 # so we multiply each cycle by 6 to get the positions for each stock
@@ -33,5 +41,3 @@ for(i in 1:length_vector){
 
 # Step 2: pulling in only adjusted prices
 joined_prices_only <- joined_prices[,adj_position]
-
-# trying to commit on github
